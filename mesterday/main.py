1f1676fcef0ea1ce8dacbfb46581040d8e3161dc
ryu <pelly.ryu@gmail.com>
2018-09-05 10:38:10 +0900

    :arrow_up: init pip, django

--- mesterday/main.py
+++ mesterday/main.py
@@ -1,4 +1,5 @@
-import os,sys
+import os, sys
+
 os.environ['DJANGO_SETTINGS_MODULE'] = 'untitled.settings'
 
 # Google App Engine imports.
@@ -6,6 +7,7 @@ from google.appengine.ext.webapp import util
 
 # Force Django to reload its settings.
 from django.conf import settings
+
 settings._target = None
 
 import django.core.handlers.wsgi
@@ -14,11 +16,11 @@ import django.db
 import django.dispatch.dispatcher
 
 # Log errors.
-#import logging
-#def log_exception(*args, **kwds):
+# import logging
+# def log_exception(*args, **kwds):
 #    logging.exception('Exception in request:')
 #
-#django.dispatch.dispatcher.connect(
+# django.dispatch.dispatcher.connect(
 #   log_exception, django.core.signals.got_request_exception)
 
 # Unregister the rollback event handler.
@@ -26,12 +28,14 @@ django.dispatch.dispatcher.disconnect(
     django.db._rollback_on_exception,
     django.core.signals.got_request_exception)
 
-def main():
-  # Create a Django application for WSGI.
-  application = django.core.handlers.wsgi.WSGIHandler()
 
-  # Run the WSGI CGI handler with that application.
-  util.run_wsgi_app(application)
+def main():
+    # Create a Django application for WSGI.
+    application = django.core.handlers.wsgi.WSGIHandler()
+
+    # Run the WSGI CGI handler with that application.
+    util.run_wsgi_app(application)
+
 
 if __name__ == '__main__':
-  main()
+    main()
